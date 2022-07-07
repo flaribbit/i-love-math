5cd77e45c4bc6118b13cc2c1706208a19f935640
flaribbit <1149761294@qq.com>
2022-07-07 10:59:37 +0800

    加强版一元二次方程

--- src/problems/12-quad2.ts
+++ src/problems/12-quad2.ts
@@ -1,13 +1,16 @@
 import { Problem } from "../types";
 import { randomInt } from "../utils";
 
-export const name = '一元二次方程';
+export const name = '一元二次方程2';
 export const get = (): Problem => {
     const x1 = randomInt(-10, 11);
     const x2 = randomInt(-10, 11);
-    const b = -x1 - x2;
+    const k1 = randomInt(1, 11);
+    const k2 = randomInt(1, 11);
+    const a = k1 * k2;
+    const b = -k1 * x2 - k2 * x1;
     const c = x1 * x2;
-    const question = `x^2 + ${b}x + ${c} = 0`
+    const question = `${a}x^2 + ${b}x + ${c} = 0`
         .replaceAll('+ -', '- ')
         .replaceAll('1x', 'x')
         .replaceAll('+ 0', '')
--- src/problems/index.ts
+++ src/problems/index.ts
@@ -7,13 +7,14 @@ import * as p05 from "./05-squared";
 import * as p06 from "./06-sqrt";
 import * as p07 from "./07-frac";
 import * as p11 from "./11-quad";
+import * as p12 from "./12-quad2";
 import * as p41 from "./41-det";
 import * as p42 from "./42-det2";
 import * as p43 from "./43-linear2";
 
 export const problems = [
     p01, p02, p03, p04, p05, p06, p07,
-    p11,
+    p11, p12,
     p41, p42, p43
 ];
 export default () => problems[randomInt(problems.length)].get();
